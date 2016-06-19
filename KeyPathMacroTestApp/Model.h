bb50f6225a88b1ab9471b22024072560aa493271
Keita Ito <keitaitok@gmail.com>
2016-06-19 01:27:49 -0700

    Add Model class

---
+++ KeyPathMacroTestApp/Model.h
@@ -0,0 +1,15 @@
+//
+//  Model.h
+//  KeyPathMacroTestApp
+//
+//  Created by Keita Ito on 6/18/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ KeyPathMacroTestApp/Model.m
@@ -0,0 +1,22 @@
+//
+//  Model.m
+//  KeyPathMacroTestApp
+//
+//  Created by Keita Ito on 6/18/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _name = @"model";
+    }
+    return self;
+}
+
+@end
