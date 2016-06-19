3cb1462f614d609f5a7acaff1b68d5b7316c6b3f
Keita Ito <keitaitok@gmail.com>
2016-06-18 22:52:42 -0700

    Initial Commit

---
+++ KeyPathMacroTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyPathMacroTestApp
+//
+//  Created by Keita Ito on 6/18/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyPathMacroTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyPathMacroTestApp
+//
+//  Created by Keita Ito on 6/18/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
